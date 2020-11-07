535dd42b81fed6aae70a33d3ea84eea8c6d2f139
Bianca Sherika Cavalcante <bianca_sherika@hotmail.com>
2020-11-07 13:13:30 -0300

    subir arquivos

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,41 @@
 const cipher = {
-  // ...
+// HIJKLMNOPQRSTUVWXYZABCDEFG
+// ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+  decode(offset, mensagem) {
+    let mensagemTraduzida = "";
+    let traduzido = "";
+    for (let inicio = 0; inicio < mensagem.length; inicio++) {
+      const traduzir = mensagem.charCodeAt(inicio);
+      if (traduzir >= 65 && traduzir <= 90) {
+        traduzido = ((traduzir + 65 - offset) % 26) + 65;
+      }
+      else if (traduzir === 32) {
+        traduzido = traduzir
+      }
+      mensagemTraduzida = mensagemTraduzida + String.fromCharCode(traduzido);
+    }
+    return mensagemTraduzida;
+  },
+
+  encode(offset3, mensagem3) {
+    let mensagemTraduzir = "";
+    let traduzido2 = "";
+    for (let inicio1 = 0; inicio1 < mensagem3.length; inicio1++) {
+      const traduzir1 = mensagem3.charCodeAt(inicio1);
+      if (traduzir1 >= 65 && traduzir1 <= 90) {
+        traduzido2 = ((traduzir1 - 65 + offset3) % 26) + 65;
+      }
+      else if (traduzir1 === 32) {
+        traduzido2 = traduzir1
+      }
+
+      mensagemTraduzir = mensagemTraduzir + String.fromCharCode(traduzido2);
+    }
+    return mensagemTraduzir;
+  }
+
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const botao = document.getElementById("botao");
+botao.addEventListener("click", chamaDecode);
+
+const botao2 = document.getElementById("botao2");
+botao2.addEventListener("click", chamaEncode);
+
+function chamaDecode() {
+    const offset1 = document.getElementById("offset").value;
+    const offset = Number(offset1);
+    const mensagem1 = document.getElementById("mensagem").value;
+    const mensagem = String(mensagem1).toUpperCase();
+    const chamar1 = cipher.decode(offset, mensagem);
+    document.getElementById("resultado").innerHTML = "A mensagem traduzida é: " + chamar1;
+}
+
+function chamaEncode() {
+    const offset2 = document.getElementById("offset2").value;
+    const offset3 = Number(offset2);
+    const mensagem2 = document.getElementById("mensagem2").value;
+    const mensagem3 = mensagem2.toUpperCase();
+    const chamar2 = cipher.encode(offset3, mensagem3);
+    document.getElementById("resultado2").innerHTML = "A mensagem traduzida é: " + chamar2;
+}
